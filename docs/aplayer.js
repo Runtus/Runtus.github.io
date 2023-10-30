91a685df0b4a27ad25afaa9258d027eaa8238a41
Runtus <893119806@qq.com>
2023-10-30 15:36:20 +0800

    Remove submodule papermod

---
+++ docs/aplayer.js
@@ -0,0 +1,9 @@
+const ap = new APlayer({
+    container: document.getElementById("aplayer"),
+    audio: [{
+        name: "踊り子",
+        artist: "Vaundy",
+        url: "https://lao-lan-go.oss-accelerate.aliyuncs.com/hugo-server/audios/Vaundy%20-%20%E8%B8%8A%E3%82%8A%E5%AD%90.mp3",
+        cover: "https://lao-lan-go.oss-accelerate.aliyuncs.com/hugo-server/audios/Vaundy.jpg"
+    }]
+})
